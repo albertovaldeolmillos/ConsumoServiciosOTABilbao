efa460787f9b853068ef821baa07138eb369f363
Alberto <alberto.valdeolmillos@gerteksa.eus>
2020-11-25 12:43:01 +0100

    prueba cambio en git

--- ConsumoServiciosWeb/ConsumoServiciosWeb/Program.cs
+++ ConsumoServiciosWeb/ConsumoServiciosWeb/Program.cs
@@ -23,7 +23,6 @@ namespace ConsumoServiciosWeb
             string XML_ReqTA = @"<ipark_in><ins_id>100001</ins_id><lic_pla>rrr</lic_pla><date>16205305062020</date><grp_id>100026</grp_id><tar_id>100001</tar_id><amou_off>5</amou_off><prov>BILBAOTAO</prov><ext_acc>tamaraclaveria7@gmail.com</ext_acc><free_time>0</free_time><vers>1.0</vers><ah>1111111111111111</ah></ipark_in>";
             string XML_ResponseTAw = clienttariff.QueryParkingOperationWithAmountSteps(XML_ReqTA);
 
-
             //Para ejecutar este servicio NO es necesario tener acceso a la VPN de desarrollo (urruti.bilbao.eus --- 320625/Gertek.2020):
             string XML_Request = "<ipark_in><f>12000873</f><city_id>40100</city_id><d>2020-02-05T17:17:27</d><vers>1.0</vers><ah>B53AB639FF6EEEB1</ah><em>BILBAOTAO</em></ipark_in>";
             integraExternalServicesRef.integraExternalServicesSoapClient client = new integraExternalServicesRef.integraExternalServicesSoapClient("integraExternalServicesSoap");
